0612aa89ad71797529ed5b52ac7a4bd9b627e7ab
Irina <Irimin@mail.ru>
2023-03-28 22:39:51 +0700

    подсчет количества строк длины меньшей или равной 3

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿const int N = 5;
+string[] s = new string[N] {"hello", 
+                            "123",
+                            "world", 
+                            ":)",
+                            "45"};
+
+int count = 0;
+
+for (int i = 0; i < s.Length; i++)
+{
+    if (s[i].ToCharArray().Length <= 3)
+    {
+        count++;
+    }
+}                            
